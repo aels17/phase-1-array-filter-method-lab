794a96171170807bf36b0420f5b72fc978efff77
Ayman <aelsayed.nh@gmail.com>
2022-02-04 11:59:16 -0500

    commit tree ce623d05fa4ad1e274cfa7b5807e055faa45ced5

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+
+
+function findMatching(drivers, list) {
+  return drivers.filter(
+    (result) => result.toLowerCase() === list.toLowerCase()
+  );
+}
+
+function fuzzyMatch(drivers, list2)
+{
+  return drivers.filter(
+    (result) => result.toLowerCase().indexOf(list2.toLowerCase()) === 0
+  );
+}
+
+function matchName(drivers, list3) {
+  return drivers.filter((result) => result.name === list3);
+}
